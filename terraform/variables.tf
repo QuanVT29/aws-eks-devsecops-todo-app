13f1ee85780427b97a02f32dd1eb0216cb2b909a
QuanVT29 <quanvota29@gmail.com>
2026-07-16 17:43:34 +0000

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,9 @@ variable "project_name" {
   description = "Name of the project used for tagging and naming resources"
   default     = "todo-app"
 }
+
+variable "mongo_uri" {
+  type        = string
+  description = "MongoDB Atlas Connection String"
+  sensitive   = true
+}
