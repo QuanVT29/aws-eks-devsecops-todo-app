76595c4cdab8a145f279d6805004535c24a88807
QuanVT29 <quanvota29@gmail.com>
2026-07-24 10:19:01 +0000

    integrate DevSecOps pipeline

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -11,9 +11,10 @@ resource "aws_ecr_repository" "frontend" {
 # Create ECR Repository for Backend Image
 resource "aws_ecr_repository" "backend" {
   name                 = "${var.project_name}-backend"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
-  image_scanning_configuration {
-    scan_on_push = true
+  # Encrypt KMS (Fix CKV_AWS_136)
+  encryption_configuration {
+    encryption_type = "KMS"
   }
 }
